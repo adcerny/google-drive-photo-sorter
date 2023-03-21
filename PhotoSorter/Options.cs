f2fc368b6ab17320a869021c2ec4e2b18f702ed6
Adam Cerny <adam.cerny@abelandcole.co.uk>
2023-03-21 12:28:44 +0000

    Add project files.

---
+++ PhotoSorter/Options.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSorter
+{
+    public class Options
+    {
+        [Option('s', Required = false, HelpText = "Source path.", Default = "\\DCIM\\Camera")]
+        public string Source { get; set; }
+        
+        [Option('d', Required = true, HelpText = "Destination path.")]
+        public string Destination { get; set; }
+
+
+
+
+    }
+}
